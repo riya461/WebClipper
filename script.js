927deb568675647ea055b8497db6f1ded322d777
Srutav Tarun <srutavtarun@gmail.com>
2023-03-04 20:06:38 +0530

    Fixed changing bookmarks

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-const swit = document.getElementById("switch");
-const current = document.getElementsByClassName("current");
-const other = document.getElementsByClassName("other");
-const hdin = document.querySelector("temp");
-const bod = document.getElementsByClassName("main")
-const bm1 = document.querySelector("bm1")
+const swit1 = document.querySelector("#switch1");
+const swit2 = document.querySelector("#switch2")
+const curr = document.querySelector(".current");
+const othe = document.querySelector(".other");
 
-// swit.onmousedown = function(){
-//     current.display = "none";
-//     other.display = "block";
-// }
-
-hdin.onclick = function(){
-    current.style.display = "block";
+swit1.onmousedown = function () {
+    curr.style.display = "none";
+    othe.style.display = "block";
 }
 
-hdin.onclick = function(){
-    bm1.innerText = "work aavada"
+swit2.onmousedown = function () {
+    curr.style.display = "block"
+    othe.style.display = "none"
 }
-console.log("oi")
