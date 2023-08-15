bc5339bba719c8d28650d972f43d50cafa5c4a08
Riya Sabu <riyasabures@gmail.com>
2023-08-16 01:40:17 +0530

    Minimized the logs in background.js

--- background.js
+++ background.js
@@ -4,30 +4,15 @@ chrome.contextMenus.create({
     contexts: ['selection'] 
   });
 
-  const url_vals = []
-  var dict = [];
 
   chrome.contextMenus.onClicked.addListener(
     ({selectionText}) => {
-        console.log(selectionText)
+        console.log("Text selected" + selectionText)
         
         chrome.tabs.query({active: true, currentWindow:true}, tabs => {
             let urlval = tabs[0].url;
-            console.log(urlval)
+            console.log("The current url " + urlval)
             
-            if (dict.includes(urlval))
-            {
-              const pos = dict.indexOf(urlval);
-              dict[pos+1].push(selectionText)
-              console.log(dict)
-            }
-            else {
-              dict.push(urlval)
-              dict.push({selectionText})
-              console.log(dict)
-            }
-        //search whether if the current web page already has any bookmarks
-        //if not add the url to the url
 
 
         })
@@ -35,29 +20,4 @@ chrome.contextMenus.create({
     }
   ) 
 
-  chrome.contextMenus.onClicked.addListener(
-    ({}) => {
-        console.log("In the context menu")
-        
-        chrome.tabs.query({active: true, currentWindow:true}, tabs => {
-            let urlval = tabs[0].url;
-            console.log(urlval);
-            if (url_vals.includes(urlval))
-            url_vals.push(urlval)
-
-            const presentUrl = dict.indexOf(tabs[0].url)
-    console.log(dict[presentUrl+1[0]]);
-
-            
-            
-        })
-        console.log(url_vals)
-       
-  });
-
-  // export function addBookmark()
-  // {
-  //   console.log("Inside fn")
-  //   const presentUrl = dict.indexOf(tabs[0].url)
-  //   return dict[presentUrl+1[0]];
-  // }
+  
